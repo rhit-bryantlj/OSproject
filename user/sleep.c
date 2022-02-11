41b08a66993dd9bc51902d9fc9a214c5a447e0c8
csse332 <brownea1@rose-hulman.edu>
2022-02-11 18:20:07 +0000

    Done with sleep

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+
+
+int main(int argc, char* argv[]) {
+    if(argc == 1) { //Print error and exit
+        fprintf(2, "usage: sleep [argument]\n");
+        exit(1);
+    }
+
+    //Convert arg to int
+    int numTicks = atoi(argv[1]);
+
+    sleep(numTicks);
+    exit(0);
+}
+
